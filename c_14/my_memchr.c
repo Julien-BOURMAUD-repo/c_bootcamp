13f5a2aaacce47ff5a872555fec809b9068f5e27
NeXuS <j.bourmaud@myskolae.fr>
2025-11-20 08:43:32 +0100

    Create my_memchr.c

---
+++ c_14/my_memchr.c
@@ -0,0 +1,15 @@
+#include <stddef.h>
+
+void *my_memchr(const void *s, int c, size_t n)
+{
+    const unsigned char *ptr = (const unsigned char *)s;
+    unsigned char target = (unsigned char)c;
+
+    for (size_t i = 0; i < n; ++i)
+    {
+        if (ptr[i] == target)
+            return (void *)(ptr + i);
+    }
+
+    return NULL;
+}
