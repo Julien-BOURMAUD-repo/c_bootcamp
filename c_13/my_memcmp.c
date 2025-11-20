30e4765803e57b327951072b9f8aa7c9763b46eb
NeXuS <j.bourmaud@myskolae.fr>
2025-11-20 08:41:40 +0100

    Create my_memcmp.c

---
+++ c_13/my_memcmp.c
@@ -0,0 +1,14 @@
+#include <stddef.h>
+
+int my_memcmp(const void *s1, const void *s2, size_t n)
+{
+    const unsigned char *p1 = (const unsigned char *)s1;
+    const unsigned char *p2 = (const unsigned char *)s2;
+
+    for (size_t i = 0; i < n; ++i) {
+        if (p1[i] != p2[i]) {
+            return (int)p1[i] - (int)p2[i];
+        }
+    }
+    return 0;
+}
