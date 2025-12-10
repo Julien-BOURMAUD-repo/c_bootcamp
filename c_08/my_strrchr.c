0dd06d557a79bbd89502bdb9cbe20882bb24b71d
NeXuS <j.bourmaud@myskolae.fr>
2025-12-10 17:53:46 +0100

    Fix character handling in my_strrchr function

--- c_08/my_strrchr.c
+++ c_08/my_strrchr.c
@@ -1,16 +1,23 @@
 #include <stddef.h>
 
+static unsigned char fix_char(int c)
+{
+    if (c > 255)
+        return *((char *)c);
+    return (unsigned char)c;
+}
+
 char *my_strrchr(const char *s, int c)
 {
+    unsigned char target = fix_char(c);
     const char *last = NULL;
 
     while (*s) {
-        if (*s == (char)c)
+        if (*s == target)
             last = s;
         s++;
     }
-
-    if (c == '\0')
+    if (target == '\0')
         return (char *)s;
 
     return (char *)last;
