3d8c3f85904f760026711882ff6488e6e6c1dd11
NeXuS <j.bourmaud@myskolae.fr>
2025-12-10 17:59:28 +0100

    Refactor my_strrchr to simplify character handling

--- c_08/my_strrchr.c
+++ c_08/my_strrchr.c
@@ -1,23 +1,16 @@
 #include <stddef.h>
 
-static unsigned char fix_char(int c)
-{
-    if (c > 255)
-        return *((char *)c);
-    return (unsigned char)c;
-}
-
 char *my_strrchr(const char *s, int c)
 {
-    unsigned char target = fix_char(c);
     const char *last = NULL;
 
     while (*s) {
-        if (*s == target)
+        if (*s == (char)c)
             last = s;
         s++;
     }
-    if (target == '\0')
+
+    if (c == '\0')
         return (char *)s;
 
     return (char *)last;
