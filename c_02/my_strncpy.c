f7dbb98da5c53a7024a456f66db0f58e6aa10eeb
NeXuS <j.bourmaud@myskolae.fr>
2025-12-10 15:31:20 +0100

    Refactor my_strncpy with for loop structure
    Refactor my_strncpy to use for loops instead of while loops.

--- c_02/my_strncpy.c
+++ c_02/my_strncpy.c
@@ -1,21 +1,14 @@
-// my_strncpy.c
 #include <stddef.h>
 
 char *my_strncpy(char *dest, const char *src, size_t n)
 {
     size_t i = 0;
 
-    // Copier jusqu'à n caractères ou jusqu'à la fin de src
-    while (i < n && src[i] != '\0') {
+    for (; i < n && src[i] != '\0'; i++)
         dest[i] = src[i];
-        i++;
-    }
 
-    // Si src est plus court que n, compléter avec des '\0'
-    while (i < n) {
+    for (; i < n; i++)
         dest[i] = '\0';
-        i++;
-    }
 
     return dest;
 }
