3946241ad8e5dd3f061cc9fb57f117648648051b
NeXuS <j.bourmaud@myskolae.fr>
2025-12-10 17:59:47 +0100

    Update my_memset.c

--- c_10/my_memset.c
+++ c_10/my_memset.c
@@ -1,19 +1,12 @@
 #include <stddef.h>
 
-static unsigned char fix_char(int c)
-{
-    if (c > 255)
-        return *((char *)c);
-    return (unsigned char)c;
-}
-
 void *my_memset(void *s, int c, size_t n)
 {
-    unsigned char value = fix_char(c);
     unsigned char *p = s;
 
-    while (n--)
-        *p++ = value;
+    while (n--) {
+        *p++ = (unsigned char)c;
+    }
 
     return s;
 }
