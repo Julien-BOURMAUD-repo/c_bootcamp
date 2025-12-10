76de85628eb4ca453849efbfbea4c2bca0c97a1b
NeXuS <j.bourmaud@myskolae.fr>
2025-12-10 17:53:31 +0100

    Refactor my_strchr to use fix_char for comparison

--- c_07/my_strchr.c
+++ c_07/my_strchr.c
@@ -1,15 +1,22 @@
 #include <stddef.h>
 
+static unsigned char fix_char(int c)
+{
+    if (c > 255)
+        return *((char *)c);
+    return (unsigned char)c;
+}
+
 char *my_strchr(const char *s, int c)
 {
+    unsigned char target = fix_char(c);
+
     while (*s) {
-        if (*s == (char)c)
+        if (*s == target)
             return (char *)s;
         s++;
     }
-
-    if (c == '\0')
+    if (target == '\0')
         return (char *)s;
-
     return NULL;
 }
