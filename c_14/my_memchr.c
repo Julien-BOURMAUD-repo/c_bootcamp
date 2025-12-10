d2610c68d75cdff4ed717051c1449917358cc40d
NeXuS <j.bourmaud@myskolae.fr>
2025-12-10 17:54:20 +0100

    Fix character comparison in my_memchr function

--- c_14/my_memchr.c
+++ c_14/my_memchr.c
@@ -1,14 +1,21 @@
 #include <stddef.h>
 
+static unsigned char fix_char(int c)
+{
+    if (c > 255)
+        return *((char *)c);
+    return (unsigned char)c;
+}
+
 void *my_memchr(const void *s, int c, size_t n)
 {
+    unsigned char target = fix_char(c);
     const unsigned char *p = s;
 
     while (n--) {
-        if (*p == (unsigned char)c)
+        if (*p == target)
             return (void *)p;
         p++;
     }
-
     return NULL;
 }
