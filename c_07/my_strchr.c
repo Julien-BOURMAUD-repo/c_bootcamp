bf2d4f9a7132481ca5b1e767991154ee50611d9e
NeXuS <j.bourmaud@myskolae.fr>
2025-12-10 17:58:58 +0100

    Simplify my_strchr by removing fix_char function

--- c_07/my_strchr.c
+++ c_07/my_strchr.c
@@ -1,22 +1,14 @@
 #include <stddef.h>
 
-static unsigned char fix_char(int c)
-{
-    if (c > 255)
-        return *((char *)c);
-    return (unsigned char)c;
-}
-
 char *my_strchr(const char *s, int c)
 {
-    unsigned char target = fix_char(c);
-
     while (*s) {
-        if (*s == target)
+        if (*s == (char)c)
             return (char *)s;
         s++;
     }
-    if (target == '\0')
+    if (c == '\0')
         return (char *)s;
+
     return NULL;
 }
